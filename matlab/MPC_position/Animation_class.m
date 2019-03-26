af1ef248a6c7e233e48fb0df9067b866bb0d3ece
hooksj <hooksj@ucla.edu>
2019-03-26 15:39:35 -0700

    Position MPC working in Matlab

--- matlab/MPC_position/Animation_class.m
+++ matlab/MPC_position/Animation_class.m
@@ -16,15 +16,16 @@ classdef Animation_class < handle
             obj.fig = figure('KeyPressFcn', @MyKeyDown, 'KeyReleaseFcn', @MyKeyUp);
             obj.fig.UserData = false(1,7);
             set(obj.fig, 'Position', get(0,'Screensize'));
-            obj.body = plot(cm(1,1),cm(2,1),'o','MarkerSize',30);
+            obj.body = plot(cm(1,1),cm(2,1),'o','MarkerSize',25,'LineWidth',5);
             hold on
             
-            obj.foot1 = plot(p1(1,1), p1(2,1), 'x','MarkerSize',30);
-            obj.foot2 = plot(p2(1,1), p2(2,1), 'x','MarkerSize',30);
-            obj.foot3 = plot(p3(1,1), p3(2,1), 'x','MarkerSize',30);
-            obj.foot4 = plot(p4(1,1), p4(2,1), 'x','MarkerSize',30);
+            obj.foot1 = plot(p1(1,1), p1(2,1), 'x','MarkerSize',25,'LineWidth',5);
+            obj.foot2 = plot(p2(1,1), p2(2,1), 'x','MarkerSize',25,'LineWidth',5);
+            obj.foot3 = plot(p3(1,1), p3(2,1), 'x','MarkerSize',25,'LineWidth',5);
+            obj.foot4 = plot(p4(1,1), p4(2,1), 'x','MarkerSize',25,'LineWidth',5);
             
-            axis([cm(1,1)-0.4 cm(1,1)+0.4 cm(2,1)-0.4 cm(2,1)+0.4])     
+            axis([cm(1,1)-0.4 cm(1,1)+0.4 cm(2,1)-0.4 cm(2,1)+0.4])   
+            axis equal
         end
         
         function update(obj,x,p1,p2,p3,p4,C)
@@ -67,8 +68,53 @@ classdef Animation_class < handle
             drawnow;
         end
         
-        function inputs = GetUserInputs(obj)
+        function [x_dot, y_dot, yaw_dot, quit] = GetUserInputs(obj)
             inputs = obj.fig.UserData;
+            
+            KEY.FORWARD = 1;
+            KEY.BACKWARD = 2;
+            KEY.LEFT = 3;
+            KEY.RIGHT = 4;
+            KEY.ROT_LEFT = 5;
+            KEY.ROT_RIGHT = 6;
+            KEY.QUIT = 7;
+
+            % Booleans determining if the robot should start decelerating
+            x_dot = 0.0;
+            y_dot = 0.0;
+            yaw_dot = 0.0;
+            quit = 0;
+
+            % ============================
+            % Check User Inputs
+            %=============================
+            if inputs(KEY.LEFT)
+                y_dot = 0.5;
+            end
+
+            if inputs(KEY.RIGHT)
+                y_dot = -0.5;
+            end
+
+            if inputs(KEY.FORWARD)
+                x_dot = 1.0;
+            end
+
+            if inputs(KEY.BACKWARD)
+                x_dot = -1.0;
+            end
+
+            if inputs(KEY.ROT_LEFT)
+                yaw_dot = 0.7;
+            end
+
+            if inputs(KEY.ROT_RIGHT)
+                yaw_dot = -0.7;
+            end
+
+            if inputs(KEY.QUIT)
+                quit = 1;
+            end
         end
         
     end
--- matlab/MPC_position/MPC_position.m
+++ matlab/MPC_position/MPC_position.m
@@ -1,168 +1,259 @@
 clear all; clc; close all;
-tic
-dt = 0.05;
-g = 9.81;
-h = 0.5;
+% ========================================================================
+% This is a high level planner that will pass footstep locations and a
+% desired CM velocity to a QP position controller. 
+% 
+% - End conditions and foot a footstep plan is created based off of forward
+%   integration of the user input commands.
+%
+% - These conditions are passed to a QP solved by gurobi to come up with a
+%   feasible CM trajectory for the given conditions.
+% 
+% USER INPUTS
+% w - Positive Y
+% s - Negative Y
+% d - Positive X
+% a - Negative X
+% j - Positive yaw
+% d - Negative yaw
+% q - Quit
+%
+% ========================================================================
+% Function to create yaw rotation matrix
+yaw_rot = @(x) [cos(x), -sin(x); sin(x) cos(x);];
 
-p1_init = [0.25; 0.25];
-p2_init = [-0.25; 0.25];
-p3_init = [-0.25; -0.25];
-p4_init = [0.25; -0.25];
+% Gait Parameters for a single leg
+swing_time = 0.2;
+stance_time = 0.2;
 
-t1 = [0.01, 0.21];
-t2 = [0.21, 0.41];
-t3 = [0.01, 0.21];
-t4 = [0.21, 0.41];
+% Timing parameters
+T_final = swing_time + stance_time + 0.1; % final time of the entire plan
+dt = 0.05; % time step of the QP
+np = int64(T_final/dt)+1;
 
-x0 = [0.0; 0.0; 0.0; 0.0; 0.0; 0.0];
+% State vector in the inertial frame
+x = zeros(6,np); % x = [x, x_dot, y, y_dot, yaw, yaw_dot]
+h = 0.5; % initial height
 
-C_init = [1.0, 1.0, 1.0, 1.0];
+% Feet position vectors
+p1 = zeros(2,np);
+p2 = zeros(2,np);
+p3 = zeros(2,np);
+p4 = zeros(2,np);
 
-vel = 0.5;
-rot_vel = 0.0;
+% feet contact indicators
+C1 = zeros(1,np);
+C2 = zeros(1,np);
+C3 = zeros(1,np);
+C4 = zeros(1,np);
 
-p1 = [0.25, 0.25, 0.25, 0.25, 0.25, 0.35, 0.35, 0.35, 0.35, 0.35, 0.35;
-      0.25, 0.25, 0.25, 0.25, 0.25, 0.25, 0.25, 0.25, 0.25, 0.25, 0.25];
-C1 = [1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 1.0, 1.0, 1.0, 0.0, 0.0];
+% velocities set by the user to determine the trajectory
+x_dot = 0.0;
+y_dot = 0.0;
+yaw_dot = 0.0;
 
-p2 = [-0.25, -0.25, -0.25, -0.25, -0.25, -0.25, -0.25, -0.25, -0.25, -0.05, -0.05;
-      0.25, 0.25, 0.25, 0.25, 0.25, 0.25, 0.25, 0.25, 0.25, 0.25, 0.25];
-C2 = [1.0, 1.0, 1.0, 1.0, 1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 1.0];
+% Gait timing 
+t1 = [0.0; swing_time];
+t2 = [stance_time-(stance_time-swing_time)*0.5; swing_time+stance_time-(stance_time-swing_time)*0.5];
+t3 = [0.0; swing_time];
+t4 = [stance_time-(stance_time-swing_time)*0.5; swing_time+stance_time-(stance_time-swing_time)*0.5];
+T1 = zeros(2,np);
+T2 = zeros(2,np);
+T3 = zeros(2,np);
+T4 = zeros(2,np);
 
-p3 = [-0.25, -0.25, -0.25, -0.25, -0.25, -0.15, -0.15, -0.15, -0.15, -0.15, -0.15;
-      -0.25, -0.25, -0.25, -0.25, -0.25, -0.25, -0.25, -0.25, -0.25, -0.25, -0.25];
-C3 = [1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 1.0, 1.0, 1.0, 0.0, 0.0];
+p1_nom = [0.25;0.15];
+p2_nom = [-0.25;0.15];
+p3_nom = [-0.25;-0.15];
+p4_nom = [0.25;-0.15];
 
-p4 = [0.25, 0.25, 0.25, 0.25, 0.25, 0.25, 0.25, 0.25, 0.25, 0.45, 0.45;
-      -0.25, -0.25, -0.25, -0.25, -0.25, -0.25, -0.25, -0.25, -0.25, -0.25, -0.25];
-C4 = [1.0, 1.0, 1.0, 1.0, 1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 1.0];
+% initial parameters
+p1(:,1) = p1_nom;
+p2(:,1) = p2_nom;
+p3(:,1) = p3_nom;
+p4(:,1) = p4_nom;
+C1(1,1) = 1.0;
+C2(1,1) = 1.0;
+C3(1,1) = 1.0;
+C4(1,1) = 1.0;
+T1(:,1) = t1;
+T2(:,1) = t2;
+T3(:,1) = t3;
+T4(:,1) = t4;
 
-x_ref = [0.0, 0.0125, 0.025, 0.0325, 0.05, 0.075, 0.1, 0.125, 0.15, 0.175, 0.2;
-         0.0, 0.15, 0.25, 0.25, 0.25, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5;
-         0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0;
-         0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0];
-     
-T = [1, dt, dt^2, dt^3, dt^4];
-dT = [0, 1, 2.0*dt, 3*dt^2, 4*dt^3];
-ddT = [0, 0, 2.0, 6.0*dt, 12.0*dt^2];
+controller = MPC_Position_Controller_class(np-1, dt, h);
+animator = Animation_class(x(1:2:3,1), p1(:,1), p2(:,1), p3(:,1), p4(:,1));
 
-Q = zeros(150,150);
-H = zeros(150,1);
-
-for i = 2:11
-    % Acceleration cost
-    Q1 = 0.00*[ddT, zeros(1,10); zeros(1,5), ddT, zeros(1,5)]'*[ddT, zeros(1,10); zeros(1,5), ddT, zeros(1,5)];
-
-    % Loading cost
-    lambda_star = 1.0/(C1(i)+C2(i)+C3(i)+C4(i));
-    Q2 = zeros(4,15);
-    Q2(:,11:14) = eye(4);
-    Q2(:,end) = -lambda_star*[C1(i);C2(i);C3(i);C4(i)];
-    Q2 = Q2'*Q2;
-
-    % Dyanamics cost
-    Q3 = [(ddT - (g/h)*T), zeros(1,5), (g/h)*[p1(1,i),p2(1,i),p3(1,i),p4(1,i)], 0;...
-          zeros(1,5), (ddT - (g/h)*T),  (g/h)*[p1(2,i),p2(2,i),p3(2,i),p4(2,i)], 0];
-    Q3 = Q3'*Q3;
-
-    % Path Regularization
-    Q4 = [T, zeros(1,10); zeros(1,5), T, zeros(1,5)];
-    Q4 = Q4'*Q4;
+while 1
     
-    Q5 = [dT, zeros(1,10); zeros(1,5), dT, zeros(1,5)];
-    Q5 = Q5'*Q5;
-
-    Q(1+(i-2)*15:15+(i-2)*15,1+(i-2)*15:15+(i-2)*15) = Q1+Q2+Q3+Q4+Q5;
+    x_accel = 0.3;
+    y_accel = 0.2;
+    yaw_accel = 2.0;
     
-    % Linear cost
-    H(1+(i-2)*15:15+(i-2)*15,1) = [-2.0*x_ref(1,(i-1))*T-2.0*x_ref(2,(i-1))*dT, ...
-                                   -2.0*x_ref(3,(i-1))*T-2.0*x_ref(4,(i-1))*dT, ...
-                                   zeros(1,4), x_ref(:,(i-1))'*x_ref(:,(i-1))];
-end
-
-
-first = 1.0;
-
-% Constraints
-C = zeros(134,150);
-
-for i = 1:10
-    if first == 1.0
-        C(1,15) = x_ref(1,1);
-        C(2,15) = x_ref(2,1);
-        C(3,15) = x_ref(3,1);
-        C(4,15) = x_ref(4,1);
-        first = 0.0;
+    [x_dot, y_dot, yaw_dot, quit] = animator.GetUserInputs();
+    
+    if quit == 1
+       break; 
     end
     
-    C(1+13*(i-1),1+15*(i-1)) = -1.0;
-    C(2+13*(i-1),2+15*(i-1)) = -1.0;
+    for i = 2:np
+        % update yaw velocity
+        if x(6,i-1) < yaw_dot
+            if abs(yaw_dot - x(6,i-1)) < yaw_accel*dt
+                x(6,i) = yaw_dot;
+            else
+                x(6,i) = x(6,i-1) + yaw_accel*dt;
+            end
+        elseif  x(6,i-1) > yaw_dot
+            if abs(yaw_dot - x(6,i-1)) < yaw_accel*dt
+                x(6,i) = yaw_dot;
+            else
+                x(6,i) = x(6,i-1) - yaw_accel*dt;
+            end
+        end
+        
+        % update yaw position
+        x(5,i) = x(5,i-1) + x(6,i)*dt;
+        
+        R = yaw_rot(x(5,i));
+        
+        % rotate user input commands into the inertial frame
+        rotated = R*[x_dot, x_accel;y_dot, y_accel];
+        x_dot = rotated(1,1);
+        y_dot = rotated(2,1);
+        %x_accel = rotated(1,2);
+        %y_accel = rotated(2,2);
+        
+        % update linear velocity
+        if x(2,i-1) < x_dot
+            if abs(x_dot - x(2,i-1)) < x_accel*dt
+                x(2,i) = x_dot;
+            else
+                x(2,i) = x(2,i-1) + x_accel*dt;
+            end
+        elseif  x(2,i-1) > x_dot
+            if abs(x_dot - x(2,i-1)) < x_accel*dt
+                x(2,i) = x_dot;
+            else
+                x(2,i) = x(2,i-1) - x_accel*dt;
+            end
+        end
+        
+        if x(4,i-1) < y_dot
+            if abs(y_dot - x(4,i-1)) < y_accel*dt
+                x(4,i) = y_dot;
+            else
+                x(4,i) = x(4,i-1) + y_accel*dt;
+            end
+        elseif  x(4,i-1) > y_dot
+            if abs(y_dot - x(4,i-1)) < y_accel*dt
+                x(4,i) = y_dot;
+            else
+                x(4,i) = x(4,i-1) - y_accel*dt;
+            end
+        end
+        
+        % update linear position
+        x(1,i) = x(1,i-1) + x(2,i)*dt;
+        x(3,i) = x(3,i-1) + x(4,i)*dt;
+        
+        % Determine the footstep locations
+        T1(:,i) = T1(:,i-1) - [dt; dt]; 
+        if T1(1,i) < 0.0
+           if C1(1,i-1) == 0
+              C1(1,i) = 1; 
+              p1(:,i) = x(1:2:3,i)+R*(p1_nom) + [x_dot;y_dot]*stance_time*0.5;
+           else
+              C1(1,i) = 0;
+           end
+           T1(1,i) = T1(2,i);
+           T1(2,i) = swing_time+stance_time - dt;
+        else
+            p1(:,i) = p1(:,i-1);
+            C1(:,i) = C1(:,i-1);
+        end
 
-    C(3+13*(i-1),6+15*(i-1)) = -1.0;
-    C(4+13*(i-1),7+15*(i-1)) = -1.0;
-
-    C(5+13*(i-1),11+15*(i-1)) = -1.0;
-    C(6+13*(i-1),11+15*(i-1)) = 1.0;
-    C(6+13*(i-1),15+15*(i-1)) = -C1(i);
-
-    C(7+13*(i-1),12+15*(i-1)) = -1.0;
-    C(8+13*(i-1),12+15*(i-1)) = 1.0;
-    C(8+13*(i-1),15+15*(i-1)) = -C2(i);
-
-    C(9+13*(i-1),13+15*(i-1)) = -1.0;
-    C(10+13*(i-1),13+15*(i-1)) = 1.0;
-    C(10+13*(i-1),15+15*(i-1)) = -C3(i);
-
-    C(11+13*(i-1),14+15*(i-1)) = -1.0;
-    C(12+13*(i-1),14+15*(i-1)) = 1.0;
-    C(12+13*(i-1),15+15*(i-1)) = -C4(i);
-
-    C(13+13*(i-1),11+15*(i-1):14+15*(i-1)) = ones(1,4);
-    C(13+13*(i-1),15+15*(i-1)) = -1;
-
-    C(14+13*(i-1),1+15*(i-1):5+15*(i-1)) = T;
-    C(15+13*(i-1),1+15*(i-1):5+15*(i-1)) = dT;
+        T2(:,i) = T2(:,i-1) - [dt; dt]; 
+        if T2(1,i) < 0.0
+           if C2(1,i-1) == 0
+              C2(1,i) = 1; 
+              p2(:,i) = x(1:2:3,i)+R*(p2_nom) + [x_dot;y_dot]*stance_time*0.5;
+           else
+              C2(1,i) = 0;
+           end
+           T2(1,i) = T2(2,i);
+           T2(2,i) = swing_time+stance_time - dt;
+        else
+            p2(:,i) = p2(:,i-1);
+            C2(:,i) = C2(:,i-1);
+        end
+        
+        T3(:,i) = T3(:,i-1) - [dt; dt]; 
+        if T3(1,i) < 0.0
+           if C3(1,i-1) == 0
+              C3(1,i) = 1; 
+              p3(:,i) = x(1:2:3,i)+R*(p3_nom) + [x_dot;y_dot]*stance_time*0.5;
+           else
+              C3(1,i) = 0;
+           end
+           T3(1,i) = T3(2,i);
+           T3(2,i) = swing_time+stance_time - dt;
+        else
+            p3(:,i) = p3(:,i-1);
+            C3(:,i) = C3(:,i-1);
+        end
+        
+        T4(:,i) = T4(:,i-1) - [dt; dt]; 
+        if T4(1,i) < 0.0
+           if C4(1,i-1) == 0
+              C4(1,i) = 1; 
+              p4(:,i) = x(1:2:3,i)+R*(p4_nom) + [x_dot;y_dot]*stance_time*0.5;
+           else
+              C4(1,i) = 0;
+           end
+           T4(1,i) = T4(2,i);
+           T4(2,i) = swing_time+stance_time - dt;
+        else
+            p4(:,i) = p4(:,i-1);
+            C4(:,i) = C4(:,i-1);
+        end
+    end  
     
-    C(16+13*(i-1),6+15*(i-1):10+15*(i-1)) = T;
-    C(17+13*(i-1),6+15*(i-1):10+15*(i-1)) = dT;
+    x(3:4,:)
+    X = controller.update(x(1:4,:), p1, p2, p3, p4, C1, C2, C3, C4); 
+    
+    x_pos = [1.0, dt, dt^2, dt^3, dt^4]*X(1:5,1);
+    x_vel = [0.0, 1.0, 2.0*dt, 3.0*dt^2, 4.0*dt^3]*X(1:5,1);
+    y_pos = [1.0, dt, dt^2, dt^3, dt^4]*X(6:10,1)
+    y_vel = [0.0, 1.0, 2.0*dt, 3.0*dt^2, 4.0*dt^3]*X(6:10,1)
+    
+%     if abs(y_vel) > 0.001
+%         y = 1
+%     end
+    
+    animator.update([x_pos;y_pos], p1(:,2), p2(:,2), p3(:,2), p4(:,2), ...
+                    [C1(1,2), C2(1,2), C3(1,2), C4(1,2)])
+    
+    % Set initial position
+    x(1:4,1) = [x_pos; x_vel; y_pos; y_vel];
+    x(5:6,1) = x(5:6,2);
+    T1(:,1) = T1(:,2);
+    T2(:,1) = T2(:,2);
+    T3(:,1) = T3(:,2);
+    T4(:,1) = T4(:,2);
+    C1(:,1) = C1(:,2);
+    C2(:,1) = C2(:,2);
+    C3(:,1) = C3(:,2);
+    C4(:,1) = C4(:,2);
+    p1(:,1) = p1(:,2);
+    p2(:,1) = p2(:,2);
+    p3(:,1) = p3(:,2);
+    p4(:,1) = p4(:,2);
+    pause(0.01);
+    
 end
 
-C(131,end) = -x_ref(1,end);
-C(132,end) = -x_ref(2,end);
-C(133,end) = -x_ref(3,end);
-C(134,end) = -x_ref(4,end);
-
-signs = ['='; '='; '='; '='; '<'; '<'; '<'; '<'; '<'; '<'; '<'; '<'; '=';];
-sense = [signs; signs; signs; signs; signs; signs; signs; signs; signs; signs; '='; '=';'=';'='];
-rhs = zeros(132,1);
-
-lb = [-5000.0*ones(10,1); 0.0; 0.0; 0.0; 0.0; 1.0];
-ub = [5000.0*ones(10,1); 1.0; 1.0; 1.0; 1.0; 1.0];
-
-LB = [lb; lb; lb; lb; lb; lb; lb; lb; lb; lb];
-UB = [ub; ub; ub; ub; ub; ub; ub; ub; ub; ub];
-
-model.Q = sparse(Q);
-model.A = sparse(C);
-model.obj = H;
-model.lb = LB;
-model.ub = UB;
-model.sense = sense;
-
-
-% Solve the MPC
-params.outputflag = 0;
-
-res = gurobi(model, params);
-toc
-
-controller = MPC_Position_Controller_class(10, dt, h);
-X = controller.update(x_ref, p1, p2, p3, p4, C1, C2, C3, C4);
-
-P = [p1; p2; p3; p4];
-Contact = [C1; C2; C3; C4];
-
-plot_solution(X, x_ref, P, Contact, dt, 10)
+close all
 
 
 
